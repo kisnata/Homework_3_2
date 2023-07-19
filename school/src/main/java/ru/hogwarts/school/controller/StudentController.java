b7b0b490e4b6489076eabd586bdd5cbb89877b44
Ната <nata.van4inova@yandex.ru>
2023-07-19 21:26:49 +0300

    Homework_3_2

---
+++ school/src/main/java/ru/hogwarts/school/controller/StudentController.java
@@ -0,0 +1,46 @@
+package ru.hogwarts.school.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.service.StudentService;
+
+import java.util.List;
+
+@RestController
+    @RequestMapping("/students")
+    public class StudentController {
+
+        private final StudentService studentService;
+
+        public StudentController(StudentService studentService) {
+
+            this.studentService = studentService;
+        }
+
+        @PostMapping
+        public Student create(@RequestBody Student student) {
+            return studentService.create(student);
+        }
+
+        @PutMapping("/{id}")
+        public Student update(@PathVariable("id") long id, @RequestBody Student student) {
+            return studentService.update(id, student);
+        }
+
+        @GetMapping("/{id}")
+        public Student get(@PathVariable("id") long id) {
+            return studentService.get(id);
+        }
+
+        @DeleteMapping("/{id}")
+        public Student delete(@PathVariable("id") long id) {
+            return studentService.delete(id);
+        }
+
+        @GetMapping
+        public List<Student> findAll(@RequestParam(required = false) Integer age){
+            return StudentService.findAll(age);
+
+    }
+
+    }
---
+++ school/src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -0,0 +1,62 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.exception.FacultyNotFoundException;
+import ru.hogwarts.school.exception.StudentNotFoundException;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.model.Student;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class StudentService {
+    private static final Map<Long, Student> students = new HashMap<>();
+    private long idGenerator = 1;
+
+
+    public Student create(Student student) {
+        student.setId(idGenerator++);
+        students.put(idGenerator, student);
+        return student;
+    }
+    public Student update(long id, Student student) {
+        if(students.containsKey(id)) {
+            Student oldStudent = students.get(id);
+            oldStudent.setName(student.getName());
+            oldStudent.setAge(student.getAge());
+            students.replace(id, oldStudent);
+            return oldStudent;
+        } else {
+            throw new StudentNotFoundException(id);
+        }
+    }
+    public Student delete(long id) {
+        if(students.containsKey(id)) {
+            return students.remove(id);
+        } else {
+            throw new StudentNotFoundException(id);
+        }
+    }
+    public Student get(long id) {
+        if(students.containsKey(id)) {
+            return students.get(id);
+        } else {
+            throw new StudentNotFoundException(id);
+        }
+    }
+    public static List<Student> findAll(Integer age) {
+        return Optional.ofNullable(age)
+                .map(a->
+                        students.values().stream()
+                                .filter(faculty -> faculty.getAge() == a)
+                )
+                .orElseGet(() -> students.values().stream())
+                .collect(Collectors.toList());
+
+        }
+    }
